bbb466a46982f4b0958a40a4bf167ea6748d015e
platisam <platisam@gmail.com>
2023-06-22 18:11:16 +0200

    creating a Movie component as part of MovieList

--- src/App.js
+++ src/App.js
@@ -130,18 +130,24 @@ function MovieList() {
   return (
     <ul className="list">
       {movies?.map((movie) => (
-        <li key={movie.imdbID}>
-          <img src={movie.Poster} alt={`${movie.Title} poster`} />
-          <h3>{movie.Title}</h3>
-          <div>
-            <p>
-              <span>🗓</span>
-              <span>{movie.Year}</span>
-            </p>
-          </div>
-        </li>
+        <Movie />
       ))}
     </ul>
+  );
+}
+
+function Movie() {
+  return (
+    <li key={movie.imdbID}>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>🗓</span>
+          <span>{movie.Year}</span>
+        </p>
+      </div>
+    </li>
   );
 }
 
