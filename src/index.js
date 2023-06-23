7f9c62b9e16f7de4baf5d117cb978f835a2be935
platisam <platisam@gmail.com>
2023-06-23 18:49:30 +0200

    creating Test component and movieRating state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 //import "./index.css";
 //import App from "./App";
 
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -13,6 +24,8 @@ root.render(
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
-    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
+
+    <Test />
   </React.StrictMode>
 );
