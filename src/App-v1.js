93b313913f3fbfa11945fcb086e49f2a1878c63d
platisam <platisam@gmail.com>
2023-06-27 00:42:21 +0200

    example how not to fetch data in React

--- src/App-v1.js
+++ src/App-v1.js
@@ -50,15 +50,9 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "ede44225";
-
 export default function App() {
-  const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
-
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-    .then((resp) => resp.json())
-    .then((data) => console.log(data));
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -1,35 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-//import "./index.css";
-//import App from "./App";
+import "./index.css";
+import App from "./App";
 
-import StarRating from "./StarRating";
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <StarRating color="green" maxRating={7} />
-      <StarRating color="brown" maxRating={6} />
-      <StarRating color="black" maxRating={12} />
-      <StarRating color="pink" maxRating={9} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+//import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating size={24} color="red" className="test" defaultRating={2} />
-
-    <Test />
+    <App />
   </React.StrictMode>
 );
