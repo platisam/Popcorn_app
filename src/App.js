b911afb5b0bfb8a2dc85fdab294b0160dc22202c
platisam <platisam@gmail.com>
2023-06-27 23:18:43 +0200

    added ErrorMessage component

--- src/App.js
+++ src/App.js
@@ -106,6 +106,14 @@ function Loader() {
   return <p className="loader">Loading...</p>;
 }
 
+function ErrorMessage({ message }) {
+  return (
+    <p className="error">
+      <span>⛔</span> {message}
+    </p>
+  );
+}
+
 function NavBar({ children }) {
   return (
     <nav className="nav-bar">
