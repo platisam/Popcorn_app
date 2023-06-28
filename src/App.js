cc7c6b12d94f3d46db935764d37e5c63589d343e
platisam <platisam@gmail.com>
2023-06-28 15:36:59 +0200

    fetching data for each movie in MovieDetails component - useEffect

--- src/App.js
+++ src/App.js
@@ -273,6 +273,17 @@ function Movie({ movie, onSelectMovie }) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  useEffect(function () {
+    async function getMovieDetails() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+      );
+      const data = await res.json();
+      console.log(data);
+    }
+    getMovieDetails();
+  }, []);
+
   return (
     <div className="details">
       <button className="btn-back" onClick={onCloseMovie}>
