f131d09ead36118cf1cdb1d7a38369a5b2e84ceb
platisam <platisam@gmail.com>
2023-06-30 22:54:50 +0200

    an example of how not to select DOM elements in React

--- src/App.js
+++ src/App.js
@@ -228,6 +228,12 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  useEffect(function () {
+    const el = document.querySelector(".search");
+    console.log(el);
+    el.focus();
+  });
+
   return (
     <input
       className="search"
