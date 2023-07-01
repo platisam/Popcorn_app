07a67dcbe9aa555d7500b7c155a6e898a59f3b2a
platisam <platisam@gmail.com>
2023-07-02 01:04:10 +0200

    creating a useKey custom hook

---
+++ src/useKey.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+
+export function useKey() {
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === "Escape") {
+          onCloseMovie();
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie]
+  );
+}
