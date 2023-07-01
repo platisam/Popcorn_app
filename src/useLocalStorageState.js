c4d3ff58bd3fb4457f3117f520c445f96c3cbcfe
platisam <platisam@gmail.com>
2023-07-02 00:50:14 +0200

    setting conditional rendering for storedValue

--- src/useLocalStorageState.js
+++ src/useLocalStorageState.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 export function useLocalStorageState(initialState, key) {
   const [value, setValue] = useState(() => {
     const storedValue = localStorage.getItem(key);
-    return JSON.parse(storedValue);
+    return storedValue ? JSON.parse(storedValue) : initialState;
   });
 
   useEffect(
