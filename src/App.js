64d2d029f0287fdca94d1f04a512c5c31138bfa0
platisam <platisam@gmail.com>
2023-07-01 15:02:43 +0200

    creating ref with useRef hook and passing to the input using ref prop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
 
 const tempMovieData = [
@@ -228,11 +228,12 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
-  useEffect(function () {
-    const el = document.querySelector(".search");
-    console.log(el);
-    el.focus();
-  });
+  const inputEl = useRef(null);
+  // useEffect(function () {
+  // const el = document.querySelector(".search");
+  // console.log(el);
+  // el.focus();
+  // });
 
   return (
     <input
@@ -241,6 +242,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
